5e6e6012051960b8afbaa50eaf2703f548262f09
Aditya Sahane <94785176+adityasahane07@users.noreply.github.com>
2024-07-07 09:56:44 +0530

    Add files via upload

---
+++ EmailAttachment1.py
@@ -0,0 +1,47 @@
+import smtplib
+import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import os.path
+
+def mail():
+    email="sender_email"
+    password="password"
+    send_to_email = 'receiver_email'
+    subject = 'Automated Email'
+    message = 'with attachment'
+
+    file_location = 'C:/APL/python projects/email automation/abc.txt'
+
+    msg = MIMEMultipart()
+    msg['From'] = email
+    msg['To'] = send_to_email
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(message, 'plain'))
+
+    # Setup the attachment
+    filename = os.path.basename(file_location)
+    attachment = open(file_location, "rb")
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(attachment.read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+    # Attach the attachment to the MIMEMultipart object
+    msg.attach(part)
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email, password)
+    text = msg.as_string()
+    server.sendmail(email, send_to_email, text)
+    server.quit()
+
+def main():
+    mail()
+
+if __name__=="__main__":
+    main()
